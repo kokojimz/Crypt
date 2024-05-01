688a53a3378631cb3e5fbd506f17b14d0178fa29
kokojimz <reynalldymarchell03@gmail.com>
2024-05-01 07:31:53 +0700

    fix: smart contract deployment issue

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/oK7Q_QEGEJYjRUzwFEoONkSX4LHlHzY2",
+      accounts: [
+        "969fdc97a485a81829f15d5dd03a8915012e057a21e09e8fa9f8f06a5e0b93fa",
+      ],
+    },
+  },
 };
